15a32372c0fde4ec78892ce19a9afb8ebd808e1e
BIKALPA LOHAR <bikalpa.0808@gmail.com>
2024-12-29 21:01:29 +0545

    add displayTodos function that modifies todos and append them into todosList

--- script.js
+++ script.js
@@ -1,22 +1,52 @@
 import { menu } from "./menu.js";
+import {displayTodos} from "./utils/displayTodos.js";
 
+const todoList = [
+  {
+    id: 1,
+    name: "Anything ...",
+    isCompleted: false,
+    isFavorite: false,
+    createAt: "2021-06-15",
+    dueDate: "2021-06-20",
+  },
+  {
+    id: 2,
+    name: "something ...",
+    isCompleted: false,
+    isFavorite: true,
+    createAt: "2021-06-15",
+    dueDate: "2021-06-20",
+  },
+  {
+    id: 3,
+    name: "let do ...",
+    isCompleted: true,
+    isFavorite: false,
+    createAt: "2021-06-15",
+    dueDate: "2021-06-20",
+  },
+  {
+    id: 4,
+    name: "work ...",
+    isCompleted: false,
+    isFavorite: false,
+    createAt: "2021-06-15",
+    dueDate: "2021-06-20",
+  },
+];
 
+let completeTodoArr = [];
+let inCompleteTodoArr = [];
 
-const todoList= [];
+let favoriteTodoArr = [];
 
+const navbar = document.getElementById("navbar");
 
-let completeTodoArr= [];
-let inCompleteTodoArr= [];
+const logo = document.createElement("h2");
+logo.innerText = "My Todo";
 
-let favoriteTodoArr= [];
-
-
-const navbar= document.getElementById("navbar");
-
-const logo= document.createElement("h2");
-logo.innerText= "My Todo";
-   
-const navUl= document.createElement("ul");
+const navUl = document.createElement("ul");
 navUl.classList.add("nav");
 
 // // nav.id="nav"
@@ -25,46 +55,35 @@ navUl.classList.add("nav");
 navbar.appendChild(logo);
 navbar.appendChild(navUl);
 
-
 menu.forEach((value) => {
+  const navigator = document.createElement("li");
+  navigator.innerText = value.name;
 
-    const navigator= document.createElement("li");
-    navigator.innerText = value.name
+  navigator.addEventListener("click", (e) => {
+    e.preventDefault();
 
-    navigator.addEventListener('click', (e) => {
-        e.preventDefault()
+    if (value.name === "Home") {
+      displayTodos(todoList);
+    } else if (value.name === "Completed") {
+      completeTodoArr = todoList.filter((item) => item.isCompleted);
 
-        
-        if(value.name === "Home"){
-            displayTodos()
+      displayTodos(completeTodoArr);
+    } else if (value.name === "Incomplete") {
+      inCompleteTodoArr = todoList.filter((item) => !item.isCompleted);
 
-        }else if(value.name === "Completed"){
-            completeTodoArr= todoList.filter((item)=> item.isCompleted)
+      displayTodos(inCompleteTodoArr);
+    } else if (value.name === "Favorite") {
+      favoriteTodoArr = todoList.filter((item) => item.isFavorite);
 
-            displayTodos(completeTodoArr)
+      displayTodos(favoriteTodoArr);
+    }
+    return;
+  });
 
-        }else if(value.name === "Incomplete"){
-            inCompleteTodoArr= todoList.filter((item)=> !item.isCompleted)
+  navUl.appendChild(navigator);
+});
 
-            displayTodos(inCompleteTodoArr)
-
-        }else if(value.name === "Favorite"){
-            favoriteTodoArr= todoList.filter((item)=> item.isFavorite)
-
-            displayTodos(favoriteTodoArr)
-        }
-        return
-    })
-
-    navUl.appendChild(navigator)
-})
-
-
-
-
-
-
-
+displayTodos(todoList);
 
 // {
 //     id: 1,
@@ -74,4 +93,3 @@ menu.forEach((value) => {
 //     createAt: "2021-06-15",
 //     dueDate: "2021-06-20",
 // }
-
--- utils/displayTodos.js
+++ utils/displayTodos.js
@@ -1,6 +1,22 @@
- 
+export const displayTodos = (todos) => {
+  const todosList = document.getElementById("todosList");
+  todosList.innerHTML= "";
 
-const displayTodos= (todos) => {
-    // converting array of object into html
-   const modifiedTodos= todos.map(createTodo)
+  // converting array of object into html
+  const modifiedTodos = todos.map(createTodo);
+console.log(modifiedTodos, "TODO")
+  //   appending each item of modifiedTodos into todosList
+  modifiedTodos.forEach((value) => {
+    todosList.appendChild(value);
+  });
+};
+
+function createTodo(value) {
+  const li= document.createElement("li");
+  li.setAttribute("class", "todo-item");
+  li.innerHTML= value.name
+
+//  const h3 = document.creat
+
+  return li
 }
